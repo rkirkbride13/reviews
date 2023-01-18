801dca524d8b4e4ca6db568a831d6fadee8df4c9
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-18 10:59:15 +0000

    Tested: if multiple frequencies fall above/below threshold they are replaced

--- review-2/spec/soundwave_filter_spec.rb
+++ review-2/spec/soundwave_filter_spec.rb
@@ -10,8 +10,16 @@ RSpec.describe 'soundwave_filter method' do
     expect(soundwave_filter([10,25,45,50,45], 15, 50)).to eq [15,25,45,50,45]
   end
 
+  it "returns a new soundwave if multiple frequencies falls below lower limit" do
+    expect(soundwave_filter([10,25,45,50,45,10,5], 15, 50)).to eq [15,25,45,50,45,15,15]
+  end
+
   it "returns a new soundwave if a frequency falls above upper limit" do
     expect(soundwave_filter([15,25,45,50,100], 15, 60)).to eq [15,25,45,50,60]
+  end
+
+  it "returns a new soundwave if multiple frequencies falls above upper limit" do
+    expect(soundwave_filter([1000,15,25,45,50,100], 15, 60)).to eq [60,15,25,45,50,60]
   end
 
 end
