b11bfd2471af75652ad06da0a6839ec349232cf1
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-18 11:24:28 +0000

    Review finished

--- review-2/spec/soundwave_filter_spec.rb
+++ review-2/spec/soundwave_filter_spec.rb
@@ -22,4 +22,8 @@ RSpec.describe 'soundwave_filter method' do
     expect(soundwave_filter([1000,15,25,45,50,100], 15, 60)).to eq [60,15,25,45,50,60]
   end
 
+  it "returns a new soundwave if there are frequencies above and below thresholds" do
+    expect(soundwave_filter([60,10,45,60,1500], 20, 50)).to eq [50,20,45,50,50]
+  end
+
 end
