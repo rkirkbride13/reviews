84bf44c06a1ffe1fb2ca4f870294b3c39dd1e7ef
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-02-15 10:53:33 +0000

    Tests ordered into correct/incorrect blocks. A test for 'to' written and passing

--- review-3/spellchecker.js
+++ review-3/spellchecker.js
@@ -1,6 +1,8 @@
 const spellchecker = (word) => {
   if (word === "a") {
     return "a";
+  } else if (word.length === 2) {
+    return "to";
   } else {
     return "~z~";
   }
--- review-3/spellchecker.test.js
+++ review-3/spellchecker.test.js
@@ -1,11 +1,19 @@
 const { spellchecker } = require("./spellchecker");
 
 describe("spellchecker", () => {
-  it("returns a correctly spelled single character word", () => {
-    expect(spellchecker("a")).toBe("a");
+  describe("correctly spelled words", () => {
+    it("returns a single character word", () => {
+      expect(spellchecker("a")).toBe("a");
+    });
+
+    it("returns a two character word", () => {
+      expect(spellchecker("to")).toBe("to");
+    });
   });
 
-  it('returns a formatted incorrectly spelled single character word', () => {
-    expect(spellchecker("z")).toBe("~z~");
-  })
+  describe("incorrectly spelled words", () => {
+    it("returns a formatted incorrectly spelled single character word", () => {
+      expect(spellchecker("z")).toBe("~z~");
+    });
+  });
 });
